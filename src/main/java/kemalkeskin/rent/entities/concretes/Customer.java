5838d7efcc009703af562b6060cc8d6f7c5e92ab
Kemalkeskin99 <dkdk999k@gmail.com>
2023-12-12 00:02:46 +0300

    first commit

---
+++ src/main/java/kemalkeskin/rent/entities/concretes/Customer.java
@@ -0,0 +1,41 @@
+package kemalkeskin.rent.entities.concretes;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "customers")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+	
+	@Id
+	@GeneratedValue(strategy =  GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "firstName")
+	private String firstName;
+	
+	@Column(name = "lastName")
+	private String lastName;
+	
+	@Column(name = "nationalIDentity")
+	private String nationalIDentity;
+	
+	@Column(name = "birthOfYear")
+	private String birthOfYear;
+	
+	
+
+}
